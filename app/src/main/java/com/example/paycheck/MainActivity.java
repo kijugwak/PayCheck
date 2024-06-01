2f7b228c0204785902d3751af0117bb740802b0e
God Ki Ju <rhkrrlwn@naver.com>
2024-06-02 02:54:26 +0900

    test

--- app/src/main/java/com/example/paycheck/MainActivity.java
+++ app/src/main/java/com/example/paycheck/MainActivity.java
@@ -19,7 +19,7 @@ import java.text.DecimalFormat;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
-
+    //test
     EditText editTextSalary;
     TextView textViewHourlyWage;
     TextView textViewDailyEarning;
