f34268647c62f4d7901ff98e0916eec11762fd98
God Ki Ju <rhkrrlwn@naver.com>
2024-06-02 16:29:50 +0900

    Splash Logo Image
    Splash Logo Image An extension of time

--- app/src/main/java/com/example/paycheck/SplashActivity.java
+++ app/src/main/java/com/example/paycheck/SplashActivity.java
@@ -7,7 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
-    private static int SPLASH_TIME_OUT = 3000; // 3초
+    private static int SPLASH_TIME_OUT = 5000; // 3초
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
